0b3ed2f8523af8dcbcbfeac0566bc6f926450a07
jrphub <jyotiranjanpattnaik@gmail.com>
2019-03-04 11:18:51 +0530

    Batch component reading from kafka topic

---
+++ src/main/java/com/gis/consumer/raw/BatchApp.java
@@ -0,0 +1,88 @@
+package com.gis.consumer.raw;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.*;
+
+public class BatchApp {
+    //default values
+    private static String brokers = "localhost:9092";
+    private static String groupId = "gis-batch-raw";
+    private static String topic = "gis-uk-crime-demo";
+
+    private static KafkaConsumer<String, String> createConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "BatchConsumer");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        return new KafkaConsumer<>(props);
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 3) {
+            brokers = args[0];
+            groupId = args[1];
+            topic = args[2];
+        }
+
+        runConsumer();
+    }
+
+    private static void runConsumer() {
+        System.out.println("Running Batch Consumer");
+        KafkaConsumer<String, String> consumer = createConsumer();
+        consumer.subscribe(Collections.singletonList(topic));
+
+        final int minBatchSize = 05;
+        List<String> buffer = new ArrayList<>();
+        try {
+            while (true) {
+                final ConsumerRecords<String, String> consumerRecords = consumer
+                        .poll(1000);
+                consumerRecords.forEach(record -> {
+                    buffer.add(record.value());
+                    System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
+                            record.key(), record.value(), record.partition(),
+                            record.offset());
+                });
+
+                if (buffer.size() >= minBatchSize) {
+                    store(buffer);
+                    consumer.commitSync();
+                    buffer.clear();
+                    break;
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+
+    private static void store(List<String> buffer) {
+        System.out.println("Storing data");
+        long startTime = System.currentTimeMillis();
+        SparkConf conf = new SparkConf().setMaster("local[*]")
+                .setAppName("BatchApp")
+                .set("spark.executor.instances", "2");
+
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        JavaRDD<String> batchRDD = jsc.parallelize(buffer);
+
+        batchRDD.saveAsTextFile("file://"
+                + "/home/jrp/GisBatchAppOutput/kafkabkp_"
+                + startTime);
+        jsc.close();
+    }
+}
